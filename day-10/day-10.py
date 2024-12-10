cff7e8b389e1ddb9a6e85b4d30660655bfb6a73e
Matt Diamant <mdiamant@squareup.com>
2024-12-10 12:22:51 -0800

    refactor day 10

--- day-10/day-10.py
+++ day-10/day-10.py
@@ -1,38 +1,22 @@
 import unittest
 
-DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-def find_trailheads(grid):
-    return [(x, y) for y, row in enumerate(grid) for x, cell in enumerate(row) if cell == 0]
-
-def check_trail(grid, x, y, new_x, new_y):
-    return 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]) and grid[new_y][new_x] == grid[y][x] + 1
-
-def traverse(grid, x, y, nines):
-    if grid[y][x] == 9: return set([(x, y)])
-    for dx, dy in DIRS:
+def traverse(grid, x, y, counter, add):
+    if grid[y][x] == 9: return add((x, y), counter)
+    for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
         new_x, new_y = x + dx, y + dy
-        if check_trail(grid, x, y, new_x, new_y):
-            nines |= traverse(grid, new_x, new_y, nines)
-    return nines
+        if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]) and grid[new_y][new_x] == grid[y][x] + 1:
+            counter = traverse(grid, new_x, new_y, counter, add)
+    return counter
 
-def traverse_2(grid, x, y, count):
-    if grid[y][x] == 9: return count + 1
-    for dx, dy in DIRS:
-        new_x, new_y = x + dx, y + dy
-        if check_trail(grid, x, y, new_x, new_y):
-            count = traverse_2(grid, new_x, new_y, count)
-    return count
-
-def solve_1(input):
+def solve(input, counter, add, len):
     grid = [list(map(int, list(line))) for line in input.split("\n")]
-    trailheads = find_trailheads(grid)
-    return sum([len(traverse(grid, x, y, set())) for x, y in trailheads])
+    trailheads = [(x, y) for y, row in enumerate(grid) for x, cell in enumerate(row) if cell == 0]
+    return sum([len(traverse(grid, x, y, counter, add)) for x, y in trailheads])
 
-def solve_2(input):
-    grid = [list(map(int, list(line))) for line in input.split("\n")]
-    trailheads = find_trailheads(grid)
-    return sum([traverse_2(grid, x, y, 0) for x, y in trailheads])
+def union(position, value): return set([position]) | value
+def set_len(s): return len(s)
+def add(_, value): return value + 1
+def int_len(s): return s
 
 with open("./example.txt")  as f: example  = f.read()
 with open("./example2.txt") as f: example2 = f.read()
@@ -40,18 +24,18 @@ with open("./input.txt")    as f: input    = f.read()
 
 class Test(unittest.TestCase):
     def test_solve_example_1(self):
-        self.assertEqual(solve_1(example), 1)
+        self.assertEqual(solve(example, set(), union, set_len), 1)
 
     def test_solve_example_1_2(self):
-        self.assertEqual(solve_1(example2), 36)
+        self.assertEqual(solve(example2, set(), union, set_len), 36)
 
     def test_solve_1(self):
-        self.assertEqual(solve_1(input), 688)
+        self.assertEqual(solve(input, set(), union, set_len), 688)
 
     def test_solve_example_2(self):
-        self.assertEqual(solve_2(example2), 81)
+        self.assertEqual(solve(example2, 0, add, int_len), 81)
 
     def test_solve_2(self):
-        self.assertEqual(solve_2(input), 1459)
+        self.assertEqual(solve(input, 0, add, int_len), 1459)
 
 unittest.main()
